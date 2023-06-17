9612877edb9d164c7f76c42a34a851481efe19dc
anurajsingh416 <112105681+anurajsingh416@users.noreply.github.com>
2023-06-17 10:59:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ function opentab(tabname){
     event.currentTarget.classList.add('active-link');
     document.getElementById(tabname).classList.add('active-tab');
 }
+
+// -------------menu-------------
+
+let close=document.getElementById('close');
+let open=document.getElementById('open');
+let navi=document.getElementsByClassName('a');
+let info=document.getElementById("info");
+const openmenu=()=>{
+    open.style.display="none";
+    close.style.display="block";
+    for(nav of navi){
+        nav.style.display="block";
+    }
+    close.style.top="8%";
+    info.style.marginTop="20%";
+}
+const closemenu=()=>{
+    open.style.display="block";
+    close.style.display="none";
+    for(nav of navi){
+        nav.style.display="none";
+    }
+    info.style.marginTop="83%";
+}
